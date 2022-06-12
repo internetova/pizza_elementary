a102750613c138ca9c34279f9ccc2bb3dfd1d2d6
Татьяна Сугина <internetova@yandex.ru>
2022-06-12 14:49:35 +0300

    update стринги для пиццы

--- lib/features/pizza/constants/pizza_strings.dart
+++ lib/features/pizza/constants/pizza_strings.dart
@@ -1,6 +1,32 @@
 class PizzaStrings {
-static const appBarTitle = 'Выберите пиццу';
-static const buttonTitleAddCart = 'В корзину';
+  static const appBarTitle = 'Выберите пиццу';
+  static const buttonTitleAddCart = 'В КОРЗИНУ ЗА ';
+  static const from = 'от ';
+  static const ingredients = 'Состав: ';
+  static const addIngredients = 'Добавить в пиццу';
+
+  /// Значения для enum
+  static const typeBacon = 'бекон';
+  static const typePepperoni = 'пикантная пепперони';
+  static const typeHam = 'ветчина';
+  static const typeTomatoes = 'свежие томаты';
+  static const typeChampignons = 'шампиньоны';
+  static const typePickledCucumbers = 'маринованные огурчики';
+  static const typeSweetPepper = 'сладкий перец';
+  static const typeRedOnion = 'красный лук';
+  static const typeGarlic = 'чеснок';
+  static const typeJalapeno = 'острый перец халапеньо';
+  static const typePineapple = 'сочный ананас';
+  static const typeItalianHerbs = 'итальянские травы';
+  static const typeCheddarAndParmesan = 'сыры чеддер и пармезан';
+  static const typeMozzarella = 'моцарелла';
+  static const typeCheeseCubes = 'кубики брынзы';
+  static const typeOlives = 'маслины';
+  static const typeChicken = 'нежный цыпленок';
+  static const typeUnknown = 'секретный ингредиент';
+  static const sizeS = 'Маленькая';
+  static const sizeM = 'Средняя';
+  static const sizeL = 'Большая';
 
   PizzaStrings._();
 }
