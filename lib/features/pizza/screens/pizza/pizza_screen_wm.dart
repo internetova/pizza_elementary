fded4b49ea56e9bdc8cb0dfa2c8a0a9bf2b990b1
Татьяна Сугина <internetova@yandex.ru>
2022-06-12 15:12:13 +0300

    modified PizzaScreenWidgetModel

--- lib/features/pizza/screens/pizza/pizza_screen_wm.dart
+++ lib/features/pizza/screens/pizza/pizza_screen_wm.dart
@@ -2,10 +2,13 @@ import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_elementary/features/app/di/app_scope.dart';
 import 'package:pizza_elementary/features/pizza/constants/pizza_strings.dart';
+import 'package:pizza_elementary/features/pizza/domain/entity/ingredients_type.dart';
 import 'package:pizza_elementary/features/pizza/domain/entity/pizza.dart';
+import 'package:pizza_elementary/features/pizza/screens/details/pizza_details_screen.dart';
 import 'package:pizza_elementary/features/pizza/screens/pizza/pizza_screen.dart';
 import 'package:pizza_elementary/features/pizza/screens/pizza/pizza_screen_model.dart';
 import 'package:pizza_elementary/features/platform/factory/platform_widgets_factory.dart';
+import 'package:pizza_elementary/util/extensions.dart';
 import 'package:provider/provider.dart';
 
 abstract class IPizzaScreenWidgetModel extends IWidgetModel {
@@ -16,10 +19,15 @@ abstract class IPizzaScreenWidgetModel extends IWidgetModel {
   PreferredSizeWidget get appBar;
 
   Future<void> refreshPizzas();
+
+  void goPizzaDetails(Pizza pizza);
+
+  String getIngredients(List<IngredientsType> ingredients);
 }
 
 PizzaScreenWidgetModel defaultPizzaScreenWidgetModelFactory(BuildContext context) {
   final appDependencies = context.read<IAppScope>();
+
   final model = PizzaScreenModel(
     appDependencies.errorHandler,
     appDependencies.pizzaService,
@@ -55,6 +63,37 @@ class PizzaScreenWidgetModel extends WidgetModel<PizzaScreen, PizzaScreenModel>
   /// Повторный запрос данных
   @override
   Future<void> refreshPizzas() async => _init();
+
+  /// Перейти на детальный экран
+  @override
+  void goPizzaDetails(Pizza pizza) {
+    widgetsFactory.openModalBottomSheet(
+      context: context,
+      builder: (context) => PizzaDetailsScreen(pizza: pizza),
+    );
+  }
+
+  /// Получить список ингредиентов пиццы в виде форматированной строки
+  @override
+  String getIngredients(List<IngredientsType> ingredients) {
+    final buffer = StringBuffer();
+
+    for (var i = 0; i < ingredients.length; i++) {
+      var str = '';
+
+      if (i == 0) {
+        str = '${ingredients[i].name.capitalize()}, ';
+      } else if (i == ingredients.length - 1) {
+        str = ingredients[i].name;
+      } else {
+        str = '${ingredients[i].name}, ';
+      }
+
+      buffer.write(str);
+    }
+
+    return buffer.toString();
+  }
 
   Future<void> _init() async {
     _pizzaState.loading();
