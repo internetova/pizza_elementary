6a42e98a77e4d7b9c00290ac2a07de1677636066
Татьяна Сугина <internetova@yandex.ru>
2022-06-12 15:05:46 +0300

    modified cupertino_widgets

--- lib/features/platform/widgets/cupertino_widgets.dart
+++ lib/features/platform/widgets/cupertino_widgets.dart
@@ -1,4 +1,7 @@
 export 'app_bar/cupertino_app_bar.dart';
 export 'bottom_navigation_bar/cupertino_bottom_navigation_bar.dart';
+export 'bottom_sheet/cupertino_bottom_sheet.dart';
+export 'bottom_sheet/cupertino_sized_box.dart';
+export 'buttons/custom_cupertino_button.dart';
 export 'loader/cupertino_loader.dart';
 export 'page_router/cupertino_page_router.dart';
