89aff2232eaf2e0d8a7d0e34c2b3c22733e12920
Tatiana Sugina <internetova@yandex.ru>
2022-07-09 18:08:19 +0300

    добавила кнопку Очистить в аппбар материал

--- lib/features/platform/widgets/app_bar/material_app_bar.dart
+++ lib/features/platform/widgets/app_bar/material_app_bar.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 
-
 class MaterialAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
+  final Widget? actionButton;
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   const MaterialAppBar({
     required this.title,
+    this.actionButton,
     Key? key,
   }) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(title),
       centerTitle: true,
+      actions: actionButton != null ? [actionButton!] : null,
     );
   }
 }
