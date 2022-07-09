f05414a7ee30a458ed74eab3022c9ec232420413
Tatiana Sugina <internetova@yandex.ru>
2022-07-09 18:11:35 +0300

    обновила список платформенных виджетов

--- lib/features/platform/widgets/material_widgets.dart
+++ lib/features/platform/widgets/material_widgets.dart
@@ -1,7 +1,7 @@
+export 'alert_dialog/material_alert_dialog.dart';
 export 'app_bar/material_app_bar.dart';
 export 'bottom_navigation_bar/material_bottom_navigation_bar.dart';
 export 'bottom_sheet/material_bottom_sheet.dart';
-export 'bottom_sheet/material_sized_box.dart';
 export 'buttons/custom_material_button.dart';
 export 'loader/material_loader.dart';
 export 'page_router/material_page_router.dart';
